61e8ebab3b2b7e7c5834bf63a65eb0653b863ba0
hahaha1010 <71594888+hahaha1010@users.noreply.github.com>
2024-05-30 00:26:42 +0900

    임시

---
+++ src/main/java/com/studygram/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.studygram.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/index")
+    public String main() {
+        return "index";
+    }
+}
